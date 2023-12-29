df0d970b0e41a2796f15087545ed695e0ec83fdd
Exxarah <sarahbeavan@gmail.com>
2023-12-29 20:56:00 +1300

    Stub abilitiesDisplay

---
+++ Assets/Scripts/Game/UI/Player/PlayerAbilityDisplayComponent.cs
@@ -0,0 +1,21 @@
+﻿using Game.Input;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI.Player
+{
+    public class PlayerAbilityDisplayComponent : APlayerInformationComponent
+    {
+        [SerializeField]
+        private FrameInputData.ActionType m_actionToDisplay;
+
+        [SerializeField]
+        private Image m_abilityImage = null;
+        
+        public override void Show(Entity.Entity playerEntity)
+        {
+            if (!isActiveAndEnabled) { return; }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Game/UI/Player/PlayerActionListComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Core.Unity.Utils;
+using Game.Input;
+using UnityEngine;
+
+namespace Game.UI.Player
+{
+    public class PlayerActionListComponent : APlayerInformationComponent
+    {
+        [SerializeField]
+        private SerializableDictionary<FrameInputData.ActionType, GameObject> m_actionDisplays =
+            new SerializableDictionary<FrameInputData.ActionType, GameObject>();
+        
+        public override void Show(Entity.Entity playerEntity)
+        {
+            foreach (KeyValuePair<FrameInputData.ActionType,GameObject> action in m_actionDisplays)
+            {
+                action.Value.SetActiveSafe(!string.IsNullOrEmpty(playerEntity.AbilitiesComponent
+                                                                            .GetAbility(action.Key)));
+            }
+        }
+    }
+}
