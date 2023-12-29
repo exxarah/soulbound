db92a6f75eff48e6580d45d0738f8516c4fd27ee
Exxarah <sarahbeavan@gmail.com>
2023-12-30 08:48:56 +1300

    Randomly select an idle animation to play

---
+++ Assets/Scripts/Game/Animations/BlendSelector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Game.Animations
+{
+    public class BlendSelector : StateMachineBehaviour
+    {
+        [SerializeField]
+        private string m_parameterName;
+
+        [SerializeField]
+        private float m_minimumValue = 0.0f;
+
+        [SerializeField]
+        private float m_maximumValue = 1.0f;
+
+        [SerializeField]
+        private AnimationCurve m_distributionCurve;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            float randomNormalised = Random.Range(0.0f, 1.0f);
+            float randomCurved = m_distributionCurve.Evaluate(randomNormalised);
+            float value = m_minimumValue + randomCurved * (m_maximumValue - m_minimumValue);
+            
+            animator.SetFloat(m_parameterName, value);
+        }
+    }
+}
