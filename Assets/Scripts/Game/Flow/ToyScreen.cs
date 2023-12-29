999f95556a821be1bef2f6dbf9b2cb6706c7a1a7
Exxarah <sarahbeavan@gmail.com>
2023-12-30 00:06:01 +1300

    Empty pause popup

--- Assets/Scripts/Game/Flow/ToyScreen.cs
+++ Assets/Scripts/Game/Flow/ToyScreen.cs
@@ -1,10 +1,15 @@
 ﻿using Core.Unity.Flow;
+using Core.Unity.Utils;
+using UnityEngine;
 using Screen = Core.Unity.Flow.Screen;
 
 namespace Game.Flow
 {
     public class ToyScreen : Screen
     {
+        [SerializeField]
+        private GameObject m_pausePopup = null;
+
         public override void OnViewEnter(ViewEnterParams viewEnterParams = null)
         {
             base.OnViewEnter(viewEnterParams);
@@ -12,6 +17,8 @@ namespace Game.Flow
             // TODO: Initialise with map-driven values. Spawn map, etc
             GameContext.Instance.GameState.Initialise();
             GameContext.Instance.GameState.OnGameEnded += OnGameEnded;
+            
+            m_pausePopup.SetActiveSafe(false);
         }
 
         private void OnGameEnded()
@@ -24,6 +31,18 @@ namespace Game.Flow
             {
                 FlowManager.Instance.Trigger("GameLost");
             }
+        }
+
+        public void _DoPause()
+        {
+            Time.timeScale = 0.0f;
+            m_pausePopup.SetActiveSafe(true);
+        }
+
+        public void _Resume()
+        {
+            Time.timeScale = 1.0f;
+            m_pausePopup.SetActiveSafe(false);
         }
     }
 }
