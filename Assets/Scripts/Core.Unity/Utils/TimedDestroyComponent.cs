fc5b2d3d727f476cb73d3f42a84d552e331cb585
Exxarah <sarahbeavan@gmail.com>
2023-12-30 14:38:04 +1300

    Upgrade toon pack to work with URP. Added effects for heal and charm

---
+++ Assets/Scripts/Core.Unity/Utils/TimedDestroyComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Core.Unity.Utils
+{
+    public class TimedDestroyComponent : MonoBehaviour
+    {
+        public void Initialise(float secondsTilDeath)
+        {
+            StartCoroutine(KillAfterWait(secondsTilDeath));
+        }
+
+        private IEnumerator KillAfterWait(float waitSeconds)
+        {
+            yield return new WaitForSeconds(waitSeconds);
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Combat/Effects/VFXEffect.cs
@@ -0,0 +1,25 @@
+﻿using Core.Unity.Utils;
+using UnityEngine;
+
+namespace Game.Combat.Effects
+{
+    [CreateAssetMenu(fileName = "VFXEffect_", menuName = "Effects/VFX Effect")]
+    public class VFXEffect : AAbilityEffect
+    {
+        [SerializeField]
+        private GameObject m_effectPrefab = null;
+
+        [SerializeField]
+        private float m_secondsTillDeath = 1.0f;
+
+        public override void ApplyToTarget(Transform target, GameObject caster)
+        {
+            GameObject effect = Instantiate(m_effectPrefab, target);
+            effect.transform.position = target.position;
+            
+            // TODO: Add effect killer component
+            TimedDestroyComponent component = effect.AddComponent<TimedDestroyComponent>();
+            component.Initialise(m_secondsTillDeath);
+        }
+    }
+}
